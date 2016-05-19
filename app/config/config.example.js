d7bdff850984ce9d62f6adc79a6609dc95793d78
Minor Solis <minorsolis@gmail.com>
2016-05-19 15:48:12 -0600

    Adding aws_s3_backup_sufix to config.example.js

--- app/config/config.example.js
+++ app/config/config.example.js
@@ -2,5 +2,6 @@ module.exports = {
   aws_key: 'aws_key_here',
   aws_secret: 'aws_secret_here',
   aws_s3_bucket: 'aws_s3_bucket',
+  aws_s3_backup_sufix: 'dump.tar',  
   file_path: '/path_to_your_file/'
 };
