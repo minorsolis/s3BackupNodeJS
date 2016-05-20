c55d2adbf9431b06c4b467eb4cb863714123ad74
Minor Solis <minorsolis@gmail.com>
2016-05-20 14:41:12 -0600

    New branch to accept the file as parameter

--- app/index.js
+++ app/index.js
@@ -8,14 +8,5 @@ var s3Api 	= require('./libs/s3/api');
 AWS.config.update({ accessKeyId: config.aws_key, secretAccessKey: config.aws_secret });
 
 // get the action
-var action = process.argv[2];
-if(action == undefined){ action = "putObject"; }
-
-// do the action
-if(action == 'putObject'){
-	s3Api.putObject(AWS,fs,config);
-}else if(action == 'copyObject'){
-	s3Api.copyObject(AWS,fs,config);
-}else{
-	console.log("Whatcha talkin bout Willis? that function don't exist");
-}
+var param1 = process.argv[2];
+s3Api.putObject(AWS,fs,config, param1);
